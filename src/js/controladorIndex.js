1bf73833743ad683e05da3c9ee957619893caf07
308 <308@academicos.cesde.edu.co>
2022-06-17 12:12:08 -0500

    Arreglos, objetos y tarea de objetos

--- src/js/controladorIndex.js
+++ src/js/controladorIndex.js
@@ -1,9 +1,386 @@
-//ENTRADAS == VARIABLES
-var nombre="JAMES RODRIGUEZ"
+//construyendo arreglos
 
-if(true){
-    var nombre="Lucho diaz"
+// 1 forma--TRadicional
+let numeros=[1,2,3,4,5]
+
+//2.forma--Orientada a objetos(poo)
+
+let numerosPoo=Array(1,2,3,4,5)
+let nombresPoo=Array('juan','andres','camila')
+
+//imprimir los elementos del arreglo
+
+console.log(numeros)
+console.log(nombresPoo)
+
+// accerder a un solo elemento
+console.log(numeros[2]-1)
+console.log(nombresPoo[1])
+
+//objetos js
+let persona={
+    nombre:'luis',
+    edad:26,
+    hinchaDelNacional:false,
+    foto:'url'
 }
-console.log(`hola ${nombre}`)
-console.log("hola")
-console.log("adios")
+console.log(persona)
+
+//acceder a un solo elemento
+
+console.log(persona.nombre)
+
+// crear un objeto   // https://www.expansion.com/fueradeserie/moda-y-caprichos/2022/05/31/6287a164e5fdea851c8b456d.html pagina de tenis
+
+let producto={
+    nombre:'camiseta',
+    precio:65000,
+    cantidad:3,
+    foto:'url',
+    Descripcion:'Camisa tipo polo negra',
+}
+
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
+let producto={
+    nombre:'',
+    precio:,
+    cantidad:,
+    foto:'url',
+    Descripcion:'',
+}
