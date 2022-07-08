c9f9907efa1d3f67bc094ce8cea1225328aa4964
308 <308@academicos.cesde.edu.co>
2022-07-08 11:27:06 -0500

    creamos columnas desde js

--- src/controllers/controladorllenado.js
+++ src/controllers/controladorllenado.js
@@ -4,7 +4,7 @@ let productosBD=[
         nombre:'Adidas Forum 84 Low ADV', //Forum84
         precio: 499990,
         cantidad:3,
-        foto:'https://firebasestorage.googleapis.com/v0/b/livtiendatcc.appspot.com/o/Forum84.png?alt=media&token=52c7ae41-64f1-4e00-80b5-4f7cd6855e15',
+        foto:['https://firebasestorage.googleapis.com/v0/b/livtiendatcc.appspot.com/o/Forum84.png?alt=media&token=52c7ae41-64f1-4e00-80b5-4f7cd6855e15','https://firebasestorage.googleapis.com/v0/b/livtiendatcc.appspot.com/o/forum84dos.webp?alt=media&token=15181af1-d067-4d5b-b0f9-fd125c11907e'],
         Descripcion:'Adidas se suma a la estética retro con sus Adidas Forum 84 Low ADV, uno de sus modelos más populares para este año, con colores blanco y crudo.'
     },
 
@@ -54,7 +54,7 @@ let productosBD=[
         nombre:'Lacoste L001', //L001
         precio:31700,
         cantidad:2,
-        foto:'https://firebasestorage.googleapis.com/v0/b/livtiendatcc.appspot.com/o/Joyeuse.jpg?alt=media&token=65e667ae-d021-4117-bee8-52b715bd3eed',
+        foto:'https://firebasestorage.googleapis.com/v0/b/livtiendatcc.appspot.com/o/L001.jpg?alt=media&token=113a1bec-0ff5-4396-a87b-0e4796bb6536',
         Descripcion:'Estas Lacoste lucen minimalistas, pero sin renunciar a la personalidad y distinción. Salpicadas por detalles de color sobre lienzo blanco y suela gruesa.',
     },
     {
@@ -215,4 +215,73 @@ let productosBD=[
 
 ]
 
-console.log(productosBD[29].foto)
+console.log(productosBD[5].nombre)
+
+
+//Recorriendo un array(arreglo) esculcar
+
+productosBD.forEach(function(producto){
+    console.log(producto.nombre)
+    console.log(producto.foto)
+
+})
+
+// PASOS PARA PINTAR (HACER RENDER) DE ETIQUETAS HTML DESDE JS --- TRAVERSING
+
+//1. cREAR UNA REFERENCIA EN MEMORIA (VARIABLE) QUE ALMACENE LA ETIQUETA SONBRE LA CUAL VOY A PINTAR (ETIQUETA PADRE O RAIZ)
+
+let fila=document.getElementById('fila')
+
+// 2. recorro el arreglo de datos y aplico traversing 
+productosBD.forEach(function(producto){
+    //2.1 creando una columana desde js
+    let columna=document.createElement('div')
+    columna.classList.add('col')
+
+    //2.2 creando tarjeta
+    let tarjeta=document.createElement('div')
+    tarjeta.classList.add('card','h1-100','text-center')
+
+    //2.3 creando la foto
+    let fotoproducto=document.createElement('img')
+    fotoproducto.classList.add('img-fluid','w-100','h-100')
+    fotoproducto.src=producto.foto
+    //2.4 creando el nombre
+    let nombreProducto=document.createElement('h3')
+    nombreProducto.textContent=producto.nombre
+    //2.5 Creando precio
+    let precioProducto=document.createElement('h2')
+    precioProducto.classList.add('fw-bold')
+    precioProducto.textContent='$'+producto.precio+'COP'
+
+    //2.6 Detectando eventos
+    columna.addEventListener('mouseover',function(){
+    fotoproducto.src=producto.foto[1]  
+    })
+    columna.addEventListener('mouseleave',function(){
+        fotoproducto.src=producto.foto
+    })
+    //3. defino los parder e hijos (Jerarquias)
+    tarjeta.appendChild(fotoproducto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precioProducto)
+    columna.appendChild(tarjeta)
+
+    fila.appendChild(columna)
+
+})
+
+function saludar (nombre){
+    console.log('hola'+nombre)
+
+}
+let nombre='juan';
+saludar('catalina');
+
+
+
+function sumar(n1,n2){
+return(n1+n2)
+}
+sumar(2,5);
+console.log(sumar(2,5))
